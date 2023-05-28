238c00164688766bf77c20187f69b9dce85c3f39
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-05-29 00:35:16 +0300

    author and magazine class

---
+++ author.rb
@@ -0,0 +1,26 @@
+class Author
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+    end
+  
+    def articles
+      Article.all.select{|article| article.author.name == self.name}
+    end
+  
+    def magazines
+      all_magazines = articles.map{ |article| article.magazine}
+      all_magazines.uniq
+    end
+  
+    def add_article(magazine, title)
+      Article.new(self, magazine, title)
+    end
+  
+    def topic_areas
+      magazines.collect{|magazine| magazine.category}.uniq
+    end
+  
+end
+
