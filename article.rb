603779c0b772d47eeeb128707f6cdf9c1440e1ab
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-05-29 00:18:22 +0300

    created classarticle

---
+++ article.rb
@@ -0,0 +1,22 @@
+class Article
+
+    attr_reader :magazine, :title, :author
+        @@all = []
+        def initialize(author, magazine, title)
+            @author = author
+            @magazine = magazine
+            @title = title
+            @@all << self
+          end
+    
+      def self.all
+        @@all
+      end
+    
+      def author
+        @author
+      end 
+      def magazine 
+        @magazine
+      end
+    end
